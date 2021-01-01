61d1752b0d63fea383a21511be03cd0bd1d6df8b
rajeski <asiajsr@gmail.com>
2021-01-01 19:09:42 +0900

    Continued coding the JavaScript-functionality in the main.js-file

--- main.js
+++ main.js
@@ -2,6 +2,15 @@
 
 const addCurrencyBtn = document.querySelector(".add-currency-btn");
 
+const currencies = [ 
+    {
+    name: "US Dollars",
+    abbreviation: "USD",
+    symbol: "\u0024",
+    flagURL: "http://www.geonames.org/flags/l/us.gif"
+}
+];
+
 // Event Listeners
 
 addCurrencyBtn.addEventListener("click", addCurrencyBtnClick);
