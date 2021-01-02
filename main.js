304312d48e09faabb5175713d2f99c296b55c49d
rajeski <asiajsr@gmail.com>
2021-01-02 17:25:12 +0900

    Continued adding additional currencies for convertion

--- main.js
+++ main.js
@@ -8,7 +8,43 @@ const currencies = [
     abbreviation: "USD",
     symbol: "\u0024",
     flagURL: "http://www.geonames.org/flags/l/us.gif"
-}
+},
+{
+    name: "Euro",
+    abbreviation: "EUR",
+    symbol: "\u20AC",
+    flagURL: "https://upload.wikimedia.org/wikipedia/commons/b/b7/Flag_of_Europe.svg"
+  },
+  {
+    name: "Japanese Yen",
+    abbreviation: "JPY",
+    symbol: "\u00A5",
+    flagURL: "http://www.geonames.org/flags/l/jp.gif"
+  },
+  {
+    name: "British Pound",
+    abbreviation: "GBP",
+    symbol: "\u00A3",
+    flagURL: "http://www.geonames.org/flags/l/uk.gif"
+  },
+  {
+    name: "Australian Dollar",
+    abbreviation: "AUD",
+    symbol: "\u0024",
+    flagURL: "http://www.geonames.org/flags/l/au.gif"
+  },
+  {
+    name: "Canadian Dollar",
+    abbreviation: "CAD",
+    symbol: "\u0024",
+    flagURL: "http://www.geonames.org/flags/l/ca.gif"
+  },
+  {
+    name: "Chinese Yuan Renminbi",
+    abbreviation: "CNY",
+    symbol: "\u00A5",
+    flagURL: "http://www.geonames.org/flags/l/cn.gif"
+  },
 ];
 
 // Event Listeners
