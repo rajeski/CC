2eadef2daefd30540933b8431fb27d84d9f23eb5
rajeski <asiajsr@gmail.com>
2021-02-08 23:12:54 +0900

    Continued coding the main.js-file

--- main.js
+++ main.js
@@ -247,13 +247,14 @@ function newCurrenciesListItem(currency) {
   addCurrencyList.querySelector(`[data-currency=${currency.abbreviation}]`).classList.add("disabled");
   const baseCurrencyRate = currencies.find(c => c.abbreviation===baseCurrency).rate;
   const exchangeRate = currency.abbreviation===baseCurrency ? 1 : (currency.rate/baseCurrencyRate).tofixed(4);
+  const inputValue = 
 
   currenciesList.insertAdjacentHTML (
     "beforeend",
     `<li class="currency ${currency.abbreviation===baseCurrency ? "base-currency" : ""}" id=${currency.abbreviation}>
     <img src=${currency.flagURL}class="flag">
     <div class="info">
-      <p class="input"><span class="currency-symbol"</span><input placeholder="0.0000"></p>
+      <p class="input"><span class="currency-symbol">${currency.symbol}</span><input placeholder="0.0000"></p>
       <p class="currency-name">JPY - Japanese Yen</p>
       <p class="base-currency-rate">1 USD = 104.12 JPY</p>
     </div>
