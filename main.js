05b06ecd09a695e33676eb86893c7b62ffc0bae5
rajeski <asiajsr@gmail.com>
2021-02-09 23:12:49 +0900

    Continued coding main.js-file

--- main.js
+++ main.js
@@ -247,7 +247,7 @@ function newCurrenciesListItem(currency) {
   addCurrencyList.querySelector(`[data-currency=${currency.abbreviation}]`).classList.add("disabled");
   const baseCurrencyRate = currencies.find(c => c.abbreviation===baseCurrency).rate;
   const exchangeRate = currency.abbreviation===baseCurrency ? 1 : (currency.rate/baseCurrencyRate).tofixed(4);
-  const inputValue = 
+  const inputValue = baseCurrencyAmount ? (baseCurrencyAmount*exchangeRate).toFixed(4)
 
   currenciesList.insertAdjacentHTML (
     "beforeend",
