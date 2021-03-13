fa0ba2e348fbd96a310eb0e9d67cbba7a065272e
rajeski <asiajsr@gmail.com>
2021-03-13 21:41:52 +0900

    Continued coding main.js-file

--- main.js
+++ main.js
@@ -238,13 +238,13 @@ function addCurrencyListClick(event) {
 currenciesList.addEventListener("click", currrenciesListClick);
 
 function currrenciesListClick(event) {
-  const clickedListItem = event.target.closest("li");
-  if(!clickedListItem.classList.contains("disabled")) {
-    const newCurrency = currencies.find(c => c.abbreviation===clickedListItem.getAttribute("data-currency"));
+  if(event.target.classList.contains("close")) {
+    const 
+  // const clickedListItem = event.target.closest("li");
+  // if(!clickedListItem.classList.contains("disabled")) {
+  //   const newCurrency = currencies.find(c => c.abbreviation===clickedListItem.getAttribute("data-currency"));
   }
 }
-
-// Recheck the above two functions 
 
 // Auxillary Functions
 
